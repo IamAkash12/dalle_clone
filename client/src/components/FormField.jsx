393c5ac8c2be4c86990a308a5e421d0c60672ec5
Akash Khurana <akashkhurana28@gmail.com>
2023-03-26 16:23:54 +0530

    Starting Form Field

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const FormField = () => {
+const FormField = (labelName, type , name, placeholder , value , handleChange, isSurpriseMe, handleSurpriseMe) => {
   return (
-    <div>
-      FormField
+    <div className='flex items-center gap-2 mb-2'>
+      
     </div>
   )
 }
